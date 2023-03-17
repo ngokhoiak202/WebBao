36841341159b939388e59945a2ab800496009814
AnhKhoi202 <2051050219khoi@ou.edu.vn>
2023-03-17 17:36:01 +0700

    web

---
+++ Web/Views/Home/group9.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Views
+{
+    public class group9Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Web/Views/bao.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Views
+{
+    public class baoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
